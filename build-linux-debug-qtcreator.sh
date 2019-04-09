c876007a78c11aabcf35c196603b866f89d5b91c
mstein <m.stein@h3-ms.com>
2019-04-09 15:10:25 +0200

    Remove remote argument from script.

--- build-linux-debug-qtcreator.sh
+++ build-linux-debug-qtcreator.sh
@@ -4,6 +4,6 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 mkdir -p "$SOURCE_DIR/../build-TypeHelper-conan"
 cd "$SOURCE_DIR/../build-TypeHelper-conan"
 
-conan install $SOURCE_DIR -pr=gcc820d -r h3ms
+conan install $SOURCE_DIR -pr=gcc820d
 source activate.sh && cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" $SOURCE_DIR
 source deactivate.sh
